1f176f12294d8ece8690b7a9588acf487a668197
Bachsau <dev@bachsau.com>
2023-03-11 22:17:39 +0100

    Update ip.php

--- ip.php
+++ ip.php
@@ -1,1 +1,13 @@
-<?php header('Content-Type: text/plain');echo isset($_GET['hostname'])?gethostbyaddr($_SERVER['REMOTE_ADDR']):$_SERVER['REMOTE_ADDR'];
+<?php
+declare(strict_types=1);
+
+header('Content-Type: text/plain; charset=US-ASCII');
+header('Cache-Control: no-store, max-age=0');
+header('X-Robots-Tag: noindex');
+
+if (array_key_exists('hostname', $_GET) && $_GET['hostname'] != '0') {
+	echo gethostbyaddr($_SERVER['REMOTE_ADDR']) . "\n";
+}
+else {
+	echo $_SERVER['REMOTE_ADDR'] . "\n";
+}
